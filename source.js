652e955df3df765d2164f99c1137f5302004aa7d
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-02-14 00:33:20 -0800

    Book form will now slide in from top, background will darken

--- source.js
+++ source.js
@@ -1,8 +1,12 @@
+const content = document.querySelector(".content");
 const readStatusButton = document.querySelector(".readStatus");
+const formContainer = document.querySelector(".formContainer");
+
+
 readStatusButton.addEventListener('click', ()=>{
-    let vari = prompt("Enter something");
-    console.log(vari);
-})
+    content.classList.add("darken");
+    giformContainer.classList.add("visible");
+});
 let myLibrary = []; // Initialize empty array of books
 
 function Book(title, author, pages, readStatus) {
@@ -10,10 +14,11 @@ function Book(title, author, pages, readStatus) {
     this.author = author,
     this.pages = pages,
     this.readStatus = readStatus
+
 };
 
 function addBookToLibrary(){
-    
+
 };
 
 addBookToLibrary();
